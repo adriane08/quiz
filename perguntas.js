1cebd79b3b465c20a4752d3b667feb5ae4ddd8d5
adriane08 <139055906+adriane08@users.noreply.github.com>
2025-08-10 20:13:49 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,93 @@
+export const perguntas = [
+    {
+        enunciado: "Qual club paranaense possui mais títulos estaduais (campeonato paranaense)?",
+        alternativas: [
+            {
+                texto: "Athletico",
+                afirmacao: [
+                    "Em 111 edições do Campeonato Paranaense, o Coritiba é o maior campeão, com 39 títulos. O Athletico vem em seguida, com 28.",
+                    
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Coritiba",
+                afirmacao: [
+                    "Em 111 edições do Campeonato Paranaense, o Coritiba é o maior campeão, com 39 títulos. O Athletico vem em seguida, com 28.",
+                    
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual clube possui mais Campeonato Brasileiro (Série A)?",
+        alternativas: [
+            {
+                texto: "Athletico Paranaense",
+                afirmacao: [
+                    "Brasileirão: Athletico-PR – 1 (2001); Coritiba – 1 (1985);",
+                    "Ambos possuem 1 título do Campeonato Brasileiro."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Coritiba",
+                afirmacao: [
+                    "Ambos possuem 1 título do Campeonato Brasileiro.",
+                    "Campeonato Brasileira: Athletico-PR – 1 (2001);  Coritiba – 1 (1985);"
+                    
+                    
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual possui mais copa do Brasil?",
+        alternativas: [
+            {
+                texto: "Athletico Paranaense",
+                afirmacao: [
+                    "O Athletico conquistou o título em 2019 e disputou mais duas finais (2013, 2021) da Copa do Brasil.",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Coritiba",
+                afirmacao: [
+                    "O Athletico conquistou o título da Copa do Brasil em 2019 e disputou mais duas finais (2013, 2021). Já o Coritiba chegou a final da Copa do Brasil em 2011 e 2012, mas não conquistou o título.",
+                   
+                   
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual dos clubes tem mais títulos internacionais?",
+        alternativas: [
+            {
+                texto: "Athletico Paranaense",
+                afirmacao: [
+                    "O Athletico possui 2 Sul-Americana. Participou de 2 finais de Libertadores da América. E ganhou a Copa Suruga Bank - 2019."
+                   
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Coritiba",
+                afirmacao: [
+                    "O Coritiba não possui títulos internacionais, apenas algumas participações internacionais, mas sem grandes destaques. Já o Athletico possui 2 Sul-Americana. Participou de 2 finais de Libertadores da América. E ganhou a Copa Suruga Bank - 2019"
+                     
+                ]
+            
+            }
+        ]
+    }
+  
+   
+        
+    
+];
